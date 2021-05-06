f9c9ff772231aa103c21c80e25971b9acb715f3c
ErfanMo77 <43669965+ErfanMo98@users.noreply.github.com>
2021-05-06 23:40:46 +0430

    Updated glfw premake file

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,7 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
-	staticruntime "on"
+	staticruntime "off"
 	
 	targetdir ("%{wks.location}/bin/" .. outputDir .. "/%{prj.name}")
 	objdir ("%{wks.location}/bin-int/" .. outputDir .. "/%{prj.name}")
